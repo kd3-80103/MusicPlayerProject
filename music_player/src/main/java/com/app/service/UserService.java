3d85a6d4f9c05537796c5ca5638165ec0007cc47
kd3-80149 <parmaranu111@gmail.com>
2024-02-07 18:07:44 +0530

    changes in signIn functionality

--- music_player/src/main/java/com/app/service/UserService.java
+++ music_player/src/main/java/com/app/service/UserService.java
@@ -1,11 +1,12 @@
 package com.app.service;
 
 import com.app.dto.SignInReqDTO;
+import com.app.dto.SignInRespDTO;
 import com.app.dto.SignUpDTO;
 
 public interface UserService {
 	//sign up
 		SignUpDTO userRegistration(SignUpDTO signupReqDTO);
 		
-		SignInReqDTO userSignin(SignInReqDTO signinReqDTO, String email);
+		SignInRespDTO userSignin(SignInReqDTO signinReqDTO);
 }
